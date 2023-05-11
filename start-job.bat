531c21ef8cd97d925bbfed879ba3fab490b10b36
Yang Liu <yliu138work@gmail.com>
2023-05-11 20:55:58 +1000

    add start job batch file called with every 5 mins

--- start-job.bat
+++ start-job.bat
@@ -1,1 +1,4 @@
-I:\currency-analyzer\.venv\Scripts\activate.bat && python I:\currency-analyzer\get-rate.py
+:loop
+call I:\currency-analyzer\.venv\Scripts\activate.bat && python I:\currency-analyzer\get-rate.py
+timeout /t 300 /nobreak
+goto :loop
