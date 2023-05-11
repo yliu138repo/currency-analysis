7a6c4dde4f1a30ed37abbd0c77c1a054109398e3
Yang Liu <yliu138work@gmail.com>
2023-05-11 21:09:58 +1000

    add more sql examples.

--- sql/currency.sql
+++ sql/currency.sql
@@ -1,5 +1,12 @@
+set search_path to public
+
 create table IF NOT EXISTS currencyRecord (
 	id serial PRIMARY KEY,
 	rate float NOT NULL,
 	time TIMESTAMP	NOT NULL
 );
+
+
+select rate, time 
+from currencyRecord cr
+order by time DESC
