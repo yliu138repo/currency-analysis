3b366dc1fab3bb9aba3161eca7dc048cf92d63b4
Yang Liu <yliu138work@gmail.com>
2023-05-16 20:10:19 +1000

    implemented the lowest value by x days, the x days are defined in notification.frequeyncy.

--- get-rate.py
+++ get-rate.py
@@ -7,7 +7,8 @@ from database import Database
 from notification import Notification
 from config import config
 
-notif = Notification(config(section="notification"))
+notif_config = config(section="notification")
+notif = Notification(notif_config)
 
 db = Database(config())
 db.connect()
@@ -42,11 +43,27 @@ if aud_rmb_rate is not None:
         select_latest_query = "select cr.time, cr.rate from currencyRecord cr order by cr.time DESC limit 1"
         cursor.execute(select_latest_query)
         latest_record = cursor.fetchone()
-        
+            
         if latest_record is not None and len(latest_record) >= 2:
             if aud_rmb_rate - latest_record[1] < 0:
                 notif.send_msg(f"Latest rate: {aud_rmb_rate} @ {timestamp}\nDropped {aud_rmb_rate - latest_record[1]}")
-                                        
+        
+        frequency = 30
+        if notif_config["frequency"] is not None:
+            frequency = notif_config["frequency"]        
+        select_lowest_value_query = f"""SELECT cr.time, cr.rate
+                                FROM currencyRecord cr
+                                WHERE  date_trunc('day', cr.time) <= date_trunc('day', current_timestamp) AND
+                                date_trunc('day', cr.time) > date_trunc('day', current_timestamp - interval '{frequency} days')
+                                order by cr.rate limit 1;
+                               """
+        cursor.execute(select_lowest_value_query)
+        lowest_record = cursor.fetchone()
+        if lowest_record is not None and len(lowest_record) >= 2:
+            print(f"Lowest rate: {lowest_record[1]} @ {lowest_record[0]} for last {frequency} days")
+            if lowest_record[1] >= aud_rmb_rate:
+                notif.send_msg(f"Lowest rate now!!! {aud_rmb_rate} @ {timestamp} for last {frequency} days\nLast lowest rate: {lowest_record[1]} @ {lowest_record[0]}\nDropped by {lowest_record[1] - aud_rmb_rate}")
+                                 
         # insert latest data into database
         cursor.execute("insert into currencyrecord(rate, time) values(%s, %s)", (aud_rmb_rate, timestamp))
         db.conn.commit()
--- sql/currency.sql
+++ sql/currency.sql
@@ -9,4 +9,16 @@ create table IF NOT EXISTS currencyRecord (
 
 select rate, time 
 from currencyRecord cr
-order by time DESC
+order by time DESC;
+
+SELECT *
+FROM currencyRecord cr
+WHERE  date_trunc('month', cr.time) <= date_trunc('month', current_timestamp) AND
+date_trunc('month', cr.time) > date_trunc('month', current_timestamp - interval '1 month')
+order by cr.time DESC;
+
+SELECT *
+FROM currencyRecord cr
+WHERE  date_trunc('day', cr.time) <= date_trunc('day', current_timestamp) AND
+date_trunc('day', cr.time) > date_trunc('day', current_timestamp - interval '3 days')
+order by cr.time DESC;
